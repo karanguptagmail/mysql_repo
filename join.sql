f12b418ea67b47c8dec1cc6e4e2497a4b7fbefc4
karanguptagmail <71269402+karanguptagmail@users.noreply.github.com>
2022-01-11 14:55:40 +0530

    Create join.sql

---
+++ join.sql
@@ -0,0 +1,9 @@
+select * from eve_date e1
+left join
+eve_date e2
+on
+e1.ename = e2.ename
+and
+( e1.edate = e2.edate - INTERVAL 1 second AND e2.edate < e1.edate)
+where e2.edate is null
+order by e1.edate;
